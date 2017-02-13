e800e3efdf27852abbab02eef68ecf9bddb11946
99hsueh <99hhsueh@gmail.com>
2017-02-13 22:12:00 +0000

    retreive info from repoOwner/repoName, json info

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,16 @@
 var request = require('request');
+var GITHUB_USER = "99hsueh";
+var GITHUB_TOKEN = "06d8e651ef44bb56bb122d208b9793516e5bd04c";
 
 console.log('Welcome to the GitHub Avatar Downloader');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  // ...
+var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+console.log(requestURL);
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
